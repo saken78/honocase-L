04c0fad61c14a8641e7fbbf2a83b0f2ef8323c87
saken78 <sakenspot@gmail.com>
2026-06-08 23:28:38 +0700

    feat(db): add Prisma query logging with winston and switch to process.env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,46 @@
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../../prisma/generated/client";
+import { winstonlogger } from "../lib/winston-logger";
 
 const adapter = new PrismaMariaDb({
-  user: Bun.env["DATABASE_USER"] || "",
-  database: Bun.env["DATABASE_NAME"] || "",
+  user: process.env["DATABASE_USER"] || "",
+  database: process.env["DATABASE_NAME"] || "",
 });
 
-const prisma = new PrismaClient({ adapter: adapter });
+const prisma = new PrismaClient({
+  adapter: adapter,
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+prisma.$on("query", (e) => {
+  winstonlogger.info(e);
+});
+
+prisma.$on("error", (e) => {
+  winstonlogger.error(e);
+});
+
+prisma.$on("info", (e) => {
+  winstonlogger.info(e);
+});
+
+prisma.$on("warn", (e) => {
+  winstonlogger.warn(e);
+});
 export { prisma };
